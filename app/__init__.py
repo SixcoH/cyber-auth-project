c4a9c6b6ecd4499de90f34f96cff7ab76882365e
SixcoH <sdlyumas@proton.me>
2026-08-13 00:29:23 +0200

    call library on init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
 # contient du code organisé
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
